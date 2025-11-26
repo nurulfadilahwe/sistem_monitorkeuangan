f034e6ead41190d3a119c2f2d8e43809c16544e9
nurulfadilahwe <223200254@almaata.ac.id>
2025-11-26 23:15:47 +0700

    update histori.php

--- histori.php
+++ histori.php
@@ -250,6 +250,7 @@ $bulan = [
 
         <!-- scripts -->
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
